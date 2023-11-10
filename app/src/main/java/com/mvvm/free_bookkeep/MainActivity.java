50d6d65a746d284f0fcdcef48aeb87cad74c8984
lingc <lingc@hantewin.com>
2023-11-10 10:51:39 +0800

    1.Hello world。

---
+++ app/src/main/java/com/mvvm/free_bookkeep/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mvvm.free_bookkeep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
